c84b224e034ea1f971a9ede9a9dc549dcef1508e
sourabh56 <sourabhchootu56@gmail.com>
2018-12-07 00:55:27 +0530

    changed routes

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,15 @@
 
 module.exports.routes = {
 
-  'POST /phonevalidator' : {controller : 'PhoneController', action : 'phoneNumber'},
-  'GET /getpincode' : {controller : 'googleApiController', action : 'getPinCode'},
-  'POST /emailvalidator' : {controller : 'PhoneController', action : 'emailValidation'},
-  'POST /uploadimage' : {controller: 'PhoneController', action : 'imageValidator'}
+  'POST /phonevalidator' : [{
+    controller : 'PhoneController', action : 'phoneNumber'
+  }],
+  'GET /getpincode' : [{
+    controller : 'googleApiController', action : 'getPinCode'
+  }],
+  'POST /emailvalidator' : [{
+    controller : 'PhoneController', action : 'emailValidation'
+  }],
+  'POST /uploadimage' : [{
+    controller: 'PhoneController', action : 'imageValidator'}]
 };
